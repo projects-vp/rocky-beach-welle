821dc3464edbdd0b5e003784b3d839fe9b5e6933
projects-vp <peschkeva@gmail.com>
2025-11-14 13:40:44 +0100

    created access token

--- rocky-beach-welle/token/get-token.js
+++ rocky-beach-welle/token/get-token.js
@@ -3,12 +3,43 @@ const request = require('request');
 const cors = require('cors');
 const app = express();
 app.use(cors());
+const axios = require('axios');
 
-
-const client_id = '2b059cb4a9c4321a4915dd189018185';
+const client_id = '42b059cb4a9c4321a4915dd189018185';
 const client_secret = '68a7cf119df74223b40d1585eb476f2a';
 
+/* Nur um den Refresh Token mir zu holen, wird später entfernt */
+app.get('/login', (req, res) => {
+  const scope = 'user-read-private user-read-email';
+  const redirect_uri = 'http://127.0.0.1:3002/callback';
 
+  const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
+  res.redirect(authUrl);
+});
+
+
+app.get('/callback', async (req, res) => {
+  const code = req.query.code;
+
+  const response = await axios.post('https://accounts.spotify.com/api/token',
+    new URLSearchParams({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: 'http://127.0.0.1:3002/callback',
+    }),
+    {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      auth: {
+        username: client_id,
+        password: client_secret,
+      },
+    }
+  );
+
+  const { access_token, refresh_token } = response.data;
+  res.send({ access_token, refresh_token });
+});
+/* Ende erster Login um mir den token zu holen */
 app.get('/refresh_token', function(req, res) {
 
   var refresh_token = req.query.refresh_token;
@@ -37,6 +68,6 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-app.listen(3001, () => {
-  console.log('Listening on 3001');
+app.listen(3002, () => {
+  console.log('Listening on 3002');
 });
