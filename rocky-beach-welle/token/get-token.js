25e208d7043e8b1f0bc1de20009245f13d842f05
projects-vp <peschkeva@gmail.com>
2025-11-14 13:55:19 +0100

    deleted first login for acces and refresh token

--- rocky-beach-welle/token/get-token.js
+++ rocky-beach-welle/token/get-token.js
@@ -8,41 +8,6 @@ const axios = require("axios");
 const client_id = "42b059cb4a9c4321a4915dd189018185";
 const client_secret = "68a7cf119df74223b40d1585eb476f2a";
 
-/* Nur um den Refresh Token mir zu holen, wird später entfernt 
-app.get('/login', (req, res) => {
-  const scope = 'user-read-private user-read-email';
-  const redirect_uri = 'http://127.0.0.1:3002/callback';
-
-  const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
-  res.redirect(authUrl);
-});
-
-
-app.get('/callback', async (req, res) => {
-  const code = req.query.code;
-
-  const response = await axios.post('https://accounts.spotify.com/api/token',
-    new URLSearchParams({
-      grant_type: 'authorization_code',
-      code: code,
-      redirect_uri: 'http://127.0.0.1:3002/callback',
-    }),
-    {
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      auth: {
-        username: client_id,
-        password: client_secret,
-      },
-    }
-  );
-
-  const { access_token, refresh_token } = response.data;
-  res.send({ access_token, refresh_token });
-
-
-});
-/* Ende erster Login um mir den token zu holen */
-
 const refreshToken =
   "AQA7h6RHUfMcOiaqXf-EKRy-zyQwwvSZjDbGrzZzKpqdPaQVA2yb9ft0MUsyThDAdV4D2EZgW4x7IlMznp4uxsa1c7g0MqkW6R0tePQhBPfnS-n4kJqJd2V45e_220aY-Qs";
 
