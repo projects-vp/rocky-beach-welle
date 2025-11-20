9ee13bfc1aca441e723cb5b09f8b725063326e96
projects-vp <peschkeva@gmail.com>
2025-11-20 13:00:08 +0100

    set offset and limit as variables, added usestate for offset, onClick function for loading more albums and add them to the previous

--- rocky-beach-welle/src/components/albums.jsx
+++ rocky-beach-welle/src/components/albums.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import Search from './search';
 
 function AlbumList({ searchValue }) {
   const [token, setToken] = useState("");
   const [albums, setAlbums] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const limit = 50; /* Anzahl Alben pro Anfrage */
 
   /* Token beziehen */
   async function fetchToken() {
@@ -17,10 +18,12 @@ function AlbumList({ searchValue }) {
     }
   }
   /* Daten der Alben beziehen */
-  async function fetchAlbums(accessToken) {
+  async function fetchAlbums(accessToken, x = 0, y = 50) {
     try {
+      let artistID = "3meJIgRw7YleJrmbpbJK6S";
+
       const res = await fetch(
-        "https://api.spotify.com/v1/artists/3meJIgRw7YleJrmbpbJK6S/albums?include_groups=album,single&market=DE&limit=50",
+        `https://api.spotify.com/v1/artists/${artistID}/albums?limit=${y}&offset=${x}`,
         {
           headers: {
             Authorization: `Bearer ${accessToken}`,
@@ -29,6 +32,7 @@ function AlbumList({ searchValue }) {
       );
 
       const data = await res.json();
+      console.log(data);
       return data.items || [];
     } catch (err) {
       console.error("Album-Fehler:", err);
@@ -54,17 +58,31 @@ function AlbumList({ searchValue }) {
   const filtered = albums.filter((album) =>
     album.name?.toLowerCase().includes(searchValue?.toLowerCase() || "")
   );
+
+  /* Funktion für Button für weitere Alben laden */
+  async function ladeAlben() {
+    const newOffset = offset + limit; /* Start zum Laden neuer Alben */
+    const moreAlbums = await fetchAlbums(token, newOffset, limit);
+    setAlbums((prev) => [
+      ...prev,
+      ...moreAlbums,
+    ]); /* neue Alben hinten anhängen */
+    setOffset(newOffset);
+  }
   /* Ausgabe der Alben als Liste */
   return (
-    <ul className="episode-list">
-      {filtered.map((album) => (
-        <li key={album.id} className="episode">
-          <img src={album.images?.[0]?.url} alt={album.name} width="100%" />
-          <p className="episode-title">{album.name}</p>
-          <p>{album.release_date}</p>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul className="episode-list">
+        {filtered.map((album) => (
+          <li key={album.id} className="episode">
+            <img src={album.images?.[0]?.url} alt={album.name} width="100%" />
+            <p className="episode-title">{album.name}</p>
+            <p>{album.release_date}</p>
+          </li>
+        ))}
+      </ul>
+      <button onClick={ladeAlben}>mehr Alben laden</button>
+    </div>
   );
 }
 
