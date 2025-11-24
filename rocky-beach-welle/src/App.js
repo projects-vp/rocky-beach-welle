55ac1511fb5243e95d80d87193fdc5f460c3c9ab
projects-vp <peschkeva@gmail.com>
2025-11-24 11:16:00 +0100

    added footer

--- rocky-beach-welle/src/App.js
+++ rocky-beach-welle/src/App.js
@@ -8,29 +8,52 @@ function App() {
   const [filterActive, setFilterActive] = useState(false);
 
   return (
-    <div className="container mt-4 background">
-      <header>
-        <h1>Rocky Beach Welle</h1>
-        <p className="subtitle">
-          Fanprojekt zu den Drei ??? - Alle Alben auf Spotify
-        </p>
-        <div className="d-flex mt-4 mb-4 gap-3 filters">
-          <Search searchValue={searchValue} setSearchValue={setSearchValue} />
-          <button
-            type="button"
-            className={`btn btn-primary ${!filterActive ? "" : "inactive"}`}
-            onClick={() => setFilterActive((prev) => !prev)}
-          >
-            {filterActive
-              ? "Sonderfolgen ausgeblendet"
-              : "Sonderfolgen eingeblendet"}
-          </button>
+    <div>
+      <div className="container mt-4 background">
+        <header>
+          <h1>Rocky Beach Welle</h1>
+          <p className="subtitle">
+            Fanprojekt zu den Drei ??? - Alle Alben auf Spotify
+          </p>
+          <div className="d-flex mt-4 mb-4 gap-3 filters">
+            <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+            <button
+              type="button"
+              className={`btn btn-primary ${!filterActive ? "" : "inactive"}`}
+              onClick={() => setFilterActive((prev) => !prev)}
+            >
+              {filterActive
+                ? "Sonderfolgen ausgeblendet"
+                : "Sonderfolgen eingeblendet"}
+            </button>
+          </div>
+        </header>
+        <main>
+          <AlbumList searchValue={searchValue} filterActive={filterActive} />
+        </main>
+      </div>
+      <footer>
+        <div className="container">
+          <p>
+            Diese Website ist ein nicht-kommerzielles Fanprojekt im Rahmen einer
+            Weiterbildung. „Die drei ???“ ist eine eingetragene Marke der
+            Franckh-Kosmos Verlags-GmbH & Co. KG. Alle Hörspiel-Cover und
+            Inhalte stammen über die offizielle Spotify-API und bleiben Eigentum
+            ihrer jeweiligen Rechteinhaber. Diese Seite steht in keiner
+            Verbindung zu Kosmos, Europa oder Spotify.
+          </p>
+          <p className="small">
+            Favicon von &nbsp;
+            <a
+              href="https://www.flaticon.com/free-icon/wave_7759987"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Freepik - Flaticon
+            </a>
+          </p>
         </div>
-      </header>
-      <main>
-        <AlbumList searchValue={searchValue} filterActive={filterActive} />
-      </main>
-      <footer></footer>
+      </footer>
     </div>
   );
 }
